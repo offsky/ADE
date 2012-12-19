1ec402ffa3ab4ad4f16cf3de135330911426f343
Jake Olefsky <offsky@gmail.com>
2012-12-19 09:58:16 -0800

    More tests

--- test/unit/phone/filtersSpec.js
+++ test/unit/phone/filtersSpec.js
@@ -12,10 +12,14 @@ describe('phone', function() {
 
 
     it('should convert phone string in a clickable link', function() {
-        expect(phoneFilter("760-230-1683")).toBe('<a href="tel:7602301683">760-230-1683</a>');
-        expect(phoneFilter("760.230.1683")).toBe('<a href="tel:7602301683">760.230.1683</a>');
-        expect(phoneFilter("760 230 1683")).toBe('<a href="tel:7602301683">760 230 1683</a>');
-        expect(phoneFilter("1 (760) 230-1683")).toBe('<a href="tel:17602301683">1 (760) 230-1683</a>');
+        expect(phoneFilter("760-555-1234")).toBe('<a href="tel:7605551234">760-555-1234</a>');
+        expect(phoneFilter("1-760-555-1234")).toBe('<a href="tel:17605551234">1-760-555-1234</a>');
+        expect(phoneFilter("760.555.1234")).toBe('<a href="tel:7605551234">760.555.1234</a>');
+        expect(phoneFilter("1.760.555.1234")).toBe('<a href="tel:17605551234">1.760.555.1234</a>');
+        expect(phoneFilter("760 555 1234")).toBe('<a href="tel:7605551234">760 555 1234</a>');
+        expect(phoneFilter("1 760 555 1234")).toBe('<a href="tel:17605551234">1 760 555 1234</a>');
+        expect(phoneFilter("555-1234")).toBe('<a href="tel:5551234">555 1234</a>');
+        expect(phoneFilter("1 (760) 555-1234")).toBe('<a href="tel:17605551234">1 (760) 555-1234</a>');
     });
 
     it('should be empty string/or entered value for invalid', function() {
