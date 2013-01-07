381752085b0435cab6aeb6b5930af185d390e672
Jake Olefsky <offsky@gmail.com>
2013-01-07 11:22:43 -0800

    Fix for empty options and using reserverd keyword

--- app/date/date_directives.js
+++ app/date/date_directives.js
@@ -81,7 +81,7 @@ adeModule.directive('adeDate', ['ADE','$compile','$timeout','$rootScope',functio
 			var options = {}; //The passed in options to the directive.
 			var editing=false;
 			var input = null;
-			var value = '';
+			var value = null;
 			var oldValue = null;
 			var exit = 0; //0=click, 1=tab, -1= shift tab, 2=return, -2=shift return. controls if you exited the field so you can focus the next field if appropriate
 
