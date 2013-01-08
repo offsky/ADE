0796b89ff67cdb5423b3ec97e1637e60d7269cfc
Jake Olefsky <offsky@gmail.com>
2013-01-08 08:44:40 -0800

    Changing paramater name to avoid using a reserved keyword
    class is a reserved keyword, so we should not use it. It causes
    problems when minifying code.

--- app/time/time_directives.js
+++ app/time/time_directives.js
@@ -181,7 +181,7 @@ adeModule.directive('adeTime', ['ADE','$compile','$timeout','$rootScope','$filte
                     timeLength = 5;
                 }
 
-                $compile('<input ade-timepop=\'{'+extraTPoptions+'}\' ng-model="adePickTime" ng-init="adePickTime='+value+'" maxlength="'+timeLength+'" type="text" class="'+options.class+'" />')($scope).insertAfter(element);
+                $compile('<input ade-timepop=\'{'+extraTPoptions+'}\' ng-model="adePickTime" ng-init="adePickTime='+value+'" maxlength="'+timeLength+'" type="text" class="'+options.className+'" />')($scope).insertAfter(element);
 
                 input = element.next('input');
                 input.focus(); //I do not know why both of these are necessary, but they are
@@ -199,7 +199,7 @@ adeModule.directive('adeTime', ['ADE','$compile','$timeout','$rootScope','$filte
             // Initialization code run for each directive instance once
             // TODO: understand why I have to return the observer and why the observer returns element
             return attrs.$observe('adeTime', function(settings) { //settings is the contents of the ade-text="" string
-                options = ADE.parseSettings(settings, {class:"input-medium"});
+                options = ADE.parseSettings(settings, {className:"input-medium"});
                 return element; //TODO: not sure what to return here
             });
         }
