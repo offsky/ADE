f1277e3e7e531a44ac0aae4fc0205c0a966e41f3
Jake Olefsky <offsky@gmail.com>
2013-01-09 12:28:46 -0800

    Simplifing length filter

--- app/length/length_filters.js
+++ app/length/length_filters.js
@@ -13,13 +13,11 @@
 adeModule.filter('length', function() {
 
 	//Minutes are passed in and a more readable (hrs mins) string is returned
-	function parseHrsMins(value) {
+	function mins2Pretty(value) {
 		// value should be passed in minutes
-		var output = '',
-			hours = 0,
-			mod = 0;
-
-		//console.log("parseHrsMins: " + value);
+		var output = '';
+		var hours = 0;
+		var mod = 0;
 
 		if(value < 60 && value > 0) {
 			output = (value === 1) ? value + 'min' : value + 'mins';
@@ -31,93 +29,43 @@ adeModule.filter('length', function() {
 			if (mod > 0 ) {
 				output += (mod === 1) ? ' ' + mod + 'min': ' ' + mod + 'mins';
 			}
-		} else {
-			//catching negative values
+		} else { //catching negative values
 			output = '';
 		}
 
 		return output;
 	}
 
-	function adjustHrsMins(value, reformatValues) {
-		var clean = parseInt(value, 10);
-
-		//console.log("addHrsMins: " + value + "; reformatValues: " + reformatValues + "; clean: " + clean);
-
-		if ((reformatValues[0].indexOf('h') !== -1) && ((clean > 1) || (clean === 1))) {
-			clean = clean*60;
-		}
-		return parseHrsMins(clean);
+	//Length was specified as a single unit (1hr or 60min). Return minutes
+	function single2Mins(value) {
+		var clean = parseFloat(value, 10);
+		if((value.indexOf('h') !== -1)) clean = clean*60;
+		return Math.round(clean);
 	}
 
-	function reformatHrsMins(value) {
-		var values = value.split(' '),
-			len = values.length,
-			cleanValues = [],
-			minutes;
-
-		//console.log("reformatHrsMins: " + value + "; Split values: " + values);
-
-		while (len--) {
-			cleanValues.unshift(parseInt(values[len], 10));
-		}
-
-		minutes = cleanValues[0]*60+cleanValues[1];
-
-		return parseHrsMins(minutes);
-	}
-
-	function padZero(number) {
-		return ((parseFloat(number) < 10) ? "0" : "") + Math.round(number);
+	//Length was specified as two units (1hrs 90mins). Return minutes
+	function double2Mins(value) {
+		var values = value.split(' ');
+		return parseFloat(values[0],10)*60+parseFloat(values[1],10);
 	}
 
 	return function(input) {
-		//console.log("+++++++ FILTER FOR " + input + " +++++");
 		if(!input) return '';
 
-		var values = ['h', 'hr', 'hour', 'hrs', 'hours', 'm', 'min', 'minute', 'mins', 'minutes'],
-			valuesLength = values.length,
-			decimalTime = input.toString().split("."),
-			clean = parseInt(input, 10),
-			output = '',
-			reformatValues = [],
-			matchCount = 0,
-			re, reFound = false;
+		var output = '';
 
+		//if it is already a number, just prettify it
+		if (!isNaN(input)) return mins2Pretty(input);
 
-		if (angular.isObject(decimalTime) && decimalTime.length === 2) {
-			var hours = parseInt(decimalTime[0], 10),
-				mins = decimalTime[1].replace(/\D/g,''),
-				minutes = parseInt(padZero((mins / Math.pow(10, mins.length))*60), 10);
-
-			return parseHrsMins((hours*60) + minutes);
-		}
-
-		if (!isNaN(input)) return parseHrsMins(input);
-
-		while (valuesLength--) {
-			re = new RegExp('\\B' + values[valuesLength] + '\\b', 'g');
-			reFound = re.test(input);
-
-			//console.log("Input: " + input + "; Found: " + reFound + "; Search Value: " + values[valuesLength]);
-
-			if (reFound) {
-				matchCount++;
-				reformatValues.push(values[valuesLength]);
-			}
-		}
-
-		//console.log("reformat values length: " + reformatValues.length + "; reformat values: " + reformatValues );
-
-		switch(matchCount) {
-			case 0:
-				output = parseHrsMins(clean);
+		var values = input.split(' ');
+		switch(values.length) {
+			case 1: //only one number specified (1hr or 60min)
+				output = single2Mins(input);
+				output = mins2Pretty(output);
 				break;
-			case 1:
-				output = adjustHrsMins(input, reformatValues);
-				break;
-			case 2:
-				output = reformatHrsMins(input);
+			case 2: //two units (1hrs 90mins)
+				output = double2Mins(input);
+				output = mins2Pretty(output);
 				break;
 			default:
 				output = '';
--- test/unit/length/filtersSpec.js
+++ test/unit/length/filtersSpec.js
@@ -1,45 +1,61 @@
 'use strict';
 
 describe('length', function() {
-    beforeEach(module('ADE'));
+	beforeEach(module('ADE'));
 
-    var lengthFilter;
+	var lengthFilter;
 
-    beforeEach(inject(function($filter) {
-        lengthFilter = $filter('length');
-    }));
+	beforeEach(inject(function($filter) {
+		lengthFilter = $filter('length');
+	}));
 
-    it('should convert minutes to hours + minutes', function() {
-        expect(lengthFilter(660)).toBe("11hrs");
-        expect(lengthFilter(65)).toBe("1hr 5mins");
-        expect(lengthFilter(100)).toBe("1hr 40mins");
-        expect(lengthFilter(1)).toBe("1min");
-        expect(lengthFilter("23h")).toBe("23hrs");
-        expect(lengthFilter("90min")).toBe("1hr 30mins");
-        expect(lengthFilter("1hrs 90mins")).toBe("2hrs 30mins");
-        expect(lengthFilter("1hours")).toBe("1hr");
-        expect(lengthFilter("1hour")).toBe("1hr");
-        expect(lengthFilter("1hrs")).toBe("1hr");
-        expect(lengthFilter("1hr")).toBe("1hr");
-        expect(lengthFilter("1h")).toBe("1hr");
-        expect(lengthFilter("1m")).toBe("1min");
-        expect(lengthFilter("1minute")).toBe("1min");
-        expect(lengthFilter("11hr")).toBe("11hrs");
-        expect(lengthFilter("1.5h")).toBe("1hr 30mins");
-        expect(lengthFilter("5.30h")).toBe("5hrs 18mins");
-        expect(lengthFilter("5.015h")).toBe("5hrs 1min");
-    });
+	it('should convert minutes to hours + minutes', function() {
+		expect(lengthFilter(660)).toBe("11hrs");
+		expect(lengthFilter(65)).toBe("1hr 5mins");
+		expect(lengthFilter(100)).toBe("1hr 40mins");
+		expect(lengthFilter(1)).toBe("1min");
+	});
 
-    it('should pass through already formatted values', function() {
-        expect(lengthFilter("11hrs")).toBe('11hrs');
-        expect(lengthFilter("1hr 5mins")).toBe('1hr 5mins');
-        expect(lengthFilter("1min")).toBe('1min');
-    });
+	it('should convert user entered strings to hours + minutes', function() {
+		expect(lengthFilter("23h")).toBe("23hrs");
+		expect(lengthFilter("90min")).toBe("1hr 30mins");
+		expect(lengthFilter("1hrs 90mins")).toBe("2hrs 30mins");
+		expect(lengthFilter("1hours")).toBe("1hr");
+		expect(lengthFilter("1hour")).toBe("1hr");
+		expect(lengthFilter("1hrs")).toBe("1hr");
+		expect(lengthFilter("1hr")).toBe("1hr");
+		expect(lengthFilter("1h")).toBe("1hr");
+		expect(lengthFilter("1m")).toBe("1min");
+		expect(lengthFilter("1.2m")).toBe("1min");
+		expect(lengthFilter("1minute")).toBe("1min");
+		expect(lengthFilter("11hr")).toBe("11hrs");
+		expect(lengthFilter("1.5h")).toBe("1hr 30mins");
+		expect(lengthFilter("5.30h")).toBe("5hrs 18mins");
+		expect(lengthFilter("5.015h")).toBe("5hrs 1min");
+		expect(lengthFilter("1.5h 5mins")).toBe("1hr 35mins");
+	});
 
-    it('should return empty string', function() {
-        expect(lengthFilter(0)).toBe("");
-        expect(lengthFilter(-1)).toBe("");
-        expect(lengthFilter("")).toBe("");
-        expect(lengthFilter("abcd")).toBe("");
-    });
+	it('should pass through already formatted values', function() {
+		expect(lengthFilter("11hrs")).toBe('11hrs');
+		expect(lengthFilter("1hr 5mins")).toBe('1hr 5mins');
+		expect(lengthFilter("1min")).toBe('1min');
+	});
+
+	xit('should work when units are out of order', function() {
+        expect(lengthFilter("5mins 1hr")).toBe('1hr 5mins');
+   });
+
+	xit('should work with days', function() {
+        expect(lengthFilter("1d")).toBe('24hrs');
+        expect(lengthFilter("1day")).toBe('24hrs');
+        expect(lengthFilter("1days")).toBe('24hrs');
+        expect(lengthFilter("1 day 1hr 1mins")).toBe('25hrs 1min');
+   });
+
+	it('should return empty string', function() {
+		expect(lengthFilter(0)).toBe("");
+		expect(lengthFilter(-1)).toBe("");
+		expect(lengthFilter("")).toBe("");
+		expect(lengthFilter("abcd")).toBe("");
+	});
 });
