906092eeeafa809506e6bbc071cf3557a7d16434
Jake Olefsky <offsky@gmail.com>
2013-02-04 09:06:29 -0800

    Long text now truncates at first line break if before specified length

--- test/unit/longtext/filtersSpec.js
+++ test/unit/longtext/filtersSpec.js
@@ -8,13 +8,19 @@ describe('date', function() {
 
 	beforeEach(inject(function($filter) {
 		longtextFilter = $filter('longtext');
-	})); 
+	}));
 
 	it('should print eclipses after a specified length', function() {
-		expect(longtextFilter("Click here to edit me", 5)).toBe('Click...');
+		expect(longtextFilter('Click here to edit me', 5)).toBe('Click...');
 	});
 
-	it('should print a full string if length is not specified', function() {
-        expect(longtextFilter("Click here to edit me")).toBe("Click here to edit me");
+	it('should print 100 chars if length is not specified', function() {
+        expect(longtextFilter('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab')).toBe('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa...');
+	});
+
+	it('should print the first line if there are new lines', function() {
+        expect(longtextFilter('first\nsecond')).toBe('first');
+        expect(longtextFilter('first\rsecond')).toBe('first');
+        expect(longtextFilter('first\r\nsecond', 4)).toBe('firs...');
 	});
 });
