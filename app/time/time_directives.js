36f98221bdcffe2b9c0af7e3472ab4914517ceee
Jake Olefsky <offsky@gmail.com>
2013-02-22 08:15:03 -0800

    Fix for time directive when no nested scopes

--- app/time/time_directives.js
+++ app/time/time_directives.js
@@ -211,7 +211,7 @@ angular.module('ADE').directive('adeTime', ['ADE', '$compile', '$filter', functi
 				ADE.setupKeys(input,saveEdit);
 
 				//because we have a nested directive, we need to digest the entire parent scope
-				if(scope.$parent.$localApply) scope.$parent.$localApply();
+				if(scope.$parent && scope.$parent.$localApply) scope.$parent.$localApply();
 				else scope.$apply();
 			});
 
