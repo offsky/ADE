3300909ab8684e9a1893f3d6059690483067ad16
Jake Olefsky <offsky@gmail.com>
2013-08-13 10:26:43 -0700

    First draft at install documentation. Build script produces zip file for easy downloading

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,12 +12,22 @@ if [ $? == 0 ] #if unit tests pass
 then
    #build passed
 	
-	cp -r dist/common/adeImg dist/build
-	
+	cp -r $BASE_DIR/../dist/common/adeImg $BASE_DIR/../dist/build
+
 	grunt test-dist
 	if [ $? == 0 ]
 	then
 		echo "Success"
+
+		echo "Making zip file"
+
+		mkdir $BASE_DIR/../dist/ade-1.2
+		cp $BASE_DIR/../INSTALL $BASE_DIR/../dist/ade-1.2
+		cp $BASE_DIR/../README.md $BASE_DIR/../dist/ade-1.2
+		cp $BASE_DIR/../LICENSE $BASE_DIR/../dist/ade-1.2
+		cp -r $BASE_DIR/../dist/build/* $BASE_DIR/../dist/ade-1.2
+		zip -j $BASE_DIR/../dist/build/ade.zip $BASE_DIR/../dist/ade-1.2/*
+		rm -rf $BASE_DIR/../dist/ade-1.2
 
 	else 
 		echo "Tests Failed"
