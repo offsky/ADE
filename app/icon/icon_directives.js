02d732282a2b144ce32b0afbaeb2a2dfd0d9768e
Jake Olefsky <offsky@gmail.com>
2013-08-26 14:59:02 -0700

    Typo

--- app/icon/icon_directives.js
+++ app/icon/icon_directives.js
@@ -181,7 +181,7 @@ angular.module('ADE').directive('adeIcon', ['ADE', '$compile', function(ADE, $co
 			//handles focus events
 			element.bind('focus', function(e) {
 				//if this is an organic focus, then do a click to make the popup appear.
-				//if this was a focus caused my myself then don't do the click
+				//if this was a focus caused by myself then don't do the click
 				if (!element.data('dontclick')) {
 					element.click();
 					return;
